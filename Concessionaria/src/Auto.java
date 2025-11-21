6dc524ebfbf2de1eaaadd4c1a08d95c93e06eee2
SamueleTermine <Samuele.termine@itsrizzoli.it>
2025-11-21 17:22:17 +0100

    Delete Concessionaria directory

--- Concessionaria/src/Auto.java
+++
@@ -1,5 +0,0 @@
-public class Auto extends Veicolo{
-    public Auto(String marca, String modello, String motore, String tipologia, int prezzo, int cilindrata, int anno) {
-        super(marca, modello, motore, tipologia, prezzo, cilindrata, anno);
-    }
-}
--- Concessionaria/src/Moto.java
+++
@@ -1,5 +0,0 @@
-public class Moto extends Veicolo{
-    public Moto(String marca, String modello, String motore, String tipologia, int prezzo, int cilindrata, int anno) {
-        super(marca, modello, motore, tipologia, prezzo, cilindrata, anno);
-    }
-}
