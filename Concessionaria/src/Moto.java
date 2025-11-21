9c486cdcde73dc5875725db5e507bf6fff2c41b8
SamueleTermine <Samuele.termine@itsrizzoli.it>
2025-11-21 17:21:12 +0100

    add project Concessionaria

---
+++ Concessionaria/src/Moto.java
@@ -0,0 +1,5 @@
+public class Moto extends Veicolo{
+    public Moto(String marca, String modello, String motore, String tipologia, int prezzo, int cilindrata, int anno) {
+        super(marca, modello, motore, tipologia, prezzo, cilindrata, anno);
+    }
+}
